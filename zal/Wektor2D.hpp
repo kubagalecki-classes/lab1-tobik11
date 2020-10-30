2d2d17e11ad5a27ba7bd7ca06e5ba69b63583f3b
tobik11 <tobik11@onet.pl>
2020-10-30 09:33:50 +0000

    added get set

--- zal/Wektor2D.hpp
+++ zal/Wektor2D.hpp
@@ -1,4 +1,50 @@
+// class Wektor2D
+// {
+//     // Tutaj napisz implementacje klasy Wektor2D
+// };
+
+
+class Informer
+{
+public:
+    Informer() { cout << "constructed" << endl; }
+    ~Informer() { cout << "destructed" << endl; }
+};
+
 class Wektor2D
 {
-    // Tutaj napisz implementacje klasy Wektor2D
+    double      x;
+    double      y;
+    Informer a;
+
+public:
+
+    Wektor2D(){ x = 0; y = 0;}
+
+
+    Wektor2D(double nx, double ny)
+    {
+        x = nx;
+        y = ny;
+        cout << x << endl;
+        cout << y << endl;
+    }
+
+    ~Wektor2D()
+    {
+        cout << x << endl;
+        cout << y << endl;
+    }
+
+    void setX(double nx) {  x = nx; }
+
+    void setY(double ny) {  y = ny; }
+
+    double getX() { return x; }
+
+    double getY() { return y; }
+
+    double norm() { return sqrt(x * x + y * y); }
+
+    void print() { cout << "x: " << x << "  y: " << y << endl; }
 };
